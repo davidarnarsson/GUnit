115c6f5f13d2c016c58c586ee74410cbd9c0428c
Davíð Arnarsson <davidarnarsson@gmail.com>
2015-04-03 11:59:11 +0200

    surpass user rule added

--- gunit-core/src/main/java/edu/chl/gunit/core/gamification/Engine.java
+++ gunit-core/src/main/java/edu/chl/gunit/core/gamification/Engine.java
@@ -12,6 +12,11 @@ import java.util.stream.Collectors;
 /**
  *
  * This needs some refactoring ...
+ *
+ * Seriously, there should only be one type of rule, they should be orderable
+ * and the resulting rule results should be directly fed into the GamificationContext
+ *
+ * This copypasta is baaaahhhd says the sheep.
  * Created by davida on 23.2.2015.
  */
 public class Engine {
@@ -28,6 +33,10 @@ public class Engine {
         List<RuleResult> out = new ArrayList<>();
         for (Rule r : rules.stream().filter(Rule::isPreRule).collect(Collectors.toList())) {
             RuleResult result = ((PreRuleStrategy) r.getStrategy()).calculate(ctx, r.getRecord());
+            if (result == null) {
+                continue;
+            }
+
             result.setRule(r.getRecord());
 
             out.add(result);
@@ -36,6 +45,11 @@ public class Engine {
         for (Rule r : rules.stream().filter(Rule::isPostRule).collect(Collectors.toList())) {
             RuleResult rr = ((PostRuleStrategy) r.getStrategy()).calculate(ctx, out);
 
+            if (rr == null) {
+                continue;
+            }
+
+            rr.setRule(r.getRecord());
             out.add(rr);
         }
         return out;
--- gunit-core/src/main/java/edu/chl/gunit/core/gamification/rules/SurpassingUserRule.java
+++ gunit-core/src/main/java/edu/chl/gunit/core/gamification/rules/SurpassingUserRule.java
@@ -1,10 +1,16 @@
 package edu.chl.gunit.core.gamification.rules;
 
+import com.google.inject.Inject;
 import edu.chl.gunit.core.data.tables.records.RuleRecord;
+import edu.chl.gunit.core.data.tables.records.UserRecord;
 import edu.chl.gunit.core.gamification.GamificationContext;
 import edu.chl.gunit.core.gamification.rules.annotations.PostRule;
+import edu.chl.gunit.core.services.UserService;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.stream.Stream;
 
 /**
  * Created by davida on 31.3.2015.
@@ -13,8 +19,38 @@ import java.util.List;
 @PostRule
 public class SurpassingUserRule implements PostRuleStrategy {
 
+    @Inject
+    UserService getUserService;
+
     @Override
     public RuleResult calculate(GamificationContext ctx, List<RuleResult> previousResults) {
+        int points = previousResults.stream().mapToInt(RuleResult::getPointsAwarded).sum();
+
+        List<UserRecord> users = getUserService.getList();
+        Optional<UserRecord> me = users.stream().filter(x -> x.getId() == ctx.getSession().getUserid()).findFirst();
+
+        if (!me.isPresent()) {
+            return null;
+        }
+
+        int newTotalPoints = me.get().getPoints() + points;
+        // get all users that are surpassed
+        Stream<UserRecord> surpassedUsers = users.stream()
+                .filter(x -> x.getPoints() >= me.get().getPoints() && x.getPoints() < newTotalPoints);
+
+        if (surpassedUsers.count() > 0) {
+            String usernames = surpassedUsers.reduce("", (a, b) -> String.format("%s, %s", a, b.getName()), (a, b) -> a + b);
+
+            if (surpassedUsers.count() > 1) {
+                int lastcomma = usernames.lastIndexOf(',');
+                usernames = String.format("%s og%s", usernames.substring(0, lastcomma-1), usernames.substring(lastcomma));
+            }
+
+            RuleResult rr = new RuleResult();
+            rr.setMessage(String.format("Vel gert! Þú fórst fram úr %s á stigaborðinu með %d stigin þín!", usernames, newTotalPoints));
+            return rr;
+        }
+
         return null;
     }
 }
